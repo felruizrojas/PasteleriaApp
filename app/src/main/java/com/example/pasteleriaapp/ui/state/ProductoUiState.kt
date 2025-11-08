34e0c26f0f6481121c20330da693fe2319e6c621
Felipe Ruiz Rojas <felruizrojas@Felipes-MacBook-Air-2.local>
2025-11-08 18:02:29 -0300

    Desarrollo de ProductoUiState [ui/state]

---
+++ app/src/main/java/com/example/pasteleriaapp/ui/state/ProductoUiState.kt
@@ -0,0 +1,12 @@
+package com.example.pasteleriaapp.ui.state
+
+import com.example.pasteleriaapp.domain.model.Producto
+
+data class ProductoUiState(
+    val estaCargando : Boolean = false,
+    val productos : List<Producto> = emptyList(),
+    val error: String? = null
+) {
+    val hayProductos : Boolean
+        get() = productos.isNotEmpty()
+}
